0ba2b441e3d681e011124b879865af78f578347d
Balakumar-cloudside <119562666+Balakumar-cloudside@users.noreply.github.com>
2023-10-26 16:15:22 +0530

    Update stg_annual_enterprise_survey01.sql

--- models/example/stg_annual_enterprise_survey01.sql
+++ models/example/stg_annual_enterprise_survey01.sql
@@ -1,6 +1,6 @@
 WITH
 survey AS (
-SELECT * FROM `cloudside-academy.dbt_sample_data.annual-enterprise-survey01`),
+SELECT * FROM `lyrical-medley-398809.dbt_sample_db.annual-enterprise-survey01`),
 
 ae AS ( 
 SELECT 
